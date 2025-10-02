26727112f405526f882113c1eb5ccb699c6f8ace
mhdrizwan95-netizen <mhd.rizwan95@gmail.com>
2025-10-02 18:58:41 +0300

    Add favicon link for RBC logo to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/rbc-logo.png" />
+        {/* other head tags */}
+      </head>
       <body>{children}</body>
     </html>
   );
